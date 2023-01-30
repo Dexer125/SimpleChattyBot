33e00fd287de76394773745c5f6a22fb4dc5f974
Timotej Kusy <timotejkusyofficial@gmail.com>
2023-01-30 15:14:03 +0100

    Publish finished project

---
+++ Topics/Working with strings/Operations and truth/src/Main.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val first = readln()
+    val second = readln()
+    val third = readln()
+
+    println(third == first + second)
+
+}
---
+++ Topics/Working with strings/The information about a person/src/Main.kt
@@ -0,0 +1,5 @@
+fun main() {
+    val input = readln().split(" ")
+    println("${input[0].first()}. ${input[1]}, ${input[2]} years old")
+
+}
